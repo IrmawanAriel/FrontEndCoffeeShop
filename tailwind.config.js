799bdfcee21f660c87980ffc72e4a0195feefd21
arielirmawan <arielirmawan@gmail.com>
2024-06-15 20:48:03 +0700

    FrontEndCoffeeShop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /**  @type {import('tailwindcss').Config}  */
 module.exports = {
   content: ["./pages/*.{html,js}"],
-  theme: { extend: 
-    {colors: {
-    'oren': '#FF8906',
-    }}, 
+  theme: { 
+    
+    extend: 
+    {
+      colors: {
+        'oren': '#FF8906',
+      },
+      screens: {
+        'sm': '640px',
+  
+        'md': '768px',
+  
+        'lg': '1024px',
+  
+        'xl': '1280px',
+  
+        '2xl': '1536px',
+      },
+    }, 
   },
   plugins: [],
 }
